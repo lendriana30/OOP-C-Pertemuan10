326ee2dd82796f7b5ec86f49158fbcc6064f1c4a
lendriana30 <yulitadalia@gmail.com>
2025-05-24 14:40:35 +0800

    Add files via upload

---
+++ oop-c-pertemuan10/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,19 @@
+
+package pertemuan10;
+
+
+/**
+ *
+ * @author Lendriana
+ * TGL 24 Mei 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
